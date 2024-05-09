241255d2a36f2e98d33715bc782d7f7ffcda09ab
fukimiyazaki <f2miyazaki@lab.miraikan.jst.go.jp>
2024-05-09 17:12:49 +0900

    add tag all_upload and reported
    Signed-off-by: fukimiyazaki <f2miyazaki@lab.miraikan.jst.go.jp>

--- submit_report.sh
+++ submit_report.sh
@@ -24,7 +24,8 @@ upload() {
         text=`python3 upload.py -f $FILE`
         if [ $? -eq 1 ]; then
             python3 notice_error.py log -e "$text" -u "$FILE"
-            url+="None"
+            url+=("None")
+            all_upload=0
         else
             url+=($text)
         fi
@@ -48,7 +49,8 @@ upload_split() {
         text=`python3 upload.py -f $item -s $folder_id`
         if [ $? -eq 1 ]; then
             python3 notice_error.py log -e "$text" -u "$item"
-            url+="None"
+            url+=("None")
+            all_upload=0
         else
             url+=($text)
         fi
@@ -57,7 +59,7 @@ upload_split() {
     done
 }
 
-cat issue_list.txt | grep -v "UPLOADED" | while read line
+cat issue_list.txt | while read line
 do
     title_file_name=`echo $line | cut -d ',' -f 1`
     body_file_name=`echo $line | cut -d ',' -f 2`
@@ -65,44 +67,54 @@ do
     list=($log)
     url=()
     log_name=()
+    all_upload=1
 
-    for item in "${list[@]}"
-    do
-        cd $logdir
-        SIZE=`du -d 0 $item | cut -f 1`
-        if [ $SIZE -gt 13000000 ]; then
-            FILE=(${item}_part_*)
-            if [ ! -e "${FILE[0]}" ]; then
-                tar -cvf - $item | split -b 10G - ${item}_part_
+    if [[ "$line" != *ALL_UPLOAD* ]]; then
+        for item in "${list[@]}"
+        do
+            cd $logdir
+            SIZE=`du -d 0 $item | cut -f 1`
+            if [ $SIZE -gt 13000000 ]; then
+                FILE=(${item}_part_*)
+                if [ ! -e "${FILE[0]}" ]; then
+                    tar -cvf - $item | split -b 10G - ${item}_part_
+                fi
+                zips=(`ls | grep ${item}_part_`)
+                echo ${zips[@]}
+                upload_split $zips $item
+            else
+                FILE="$item.tar"
+                if [ ! -e $FILE ]; then
+                    tar -cvf $FILE $item
+                fi
+                upload $FILE
             fi
-            zips=(`ls | grep ${item}_part_`)
-            echo ${zips[@]}
-            upload_split $zips $item
-        else
-            FILE="$item.tar"
-            if [ ! -e $FILE ]; then
-                tar -cvf $FILE $item
-            fi
-            upload $FILE
+        done
+        if [[ $all_upload eq 1 ]]; then
+            sed -i "s/\(.*$log\)/\1,ALL_UPLOAD/" $scriptdir/issue_list.txt
         fi
-    done
-
-    mkdir -p $scriptdir/content
-    mkdir -p $scriptdir/error
-
-    title_path=$scriptdir/content/$title_file_name
-    file_path=$scriptdir/content/$body_file_name
-    response=`python3 make_issue.py -t $title_path -f $file_path -u ${url[@]} -l ${log_name[@]}`
-
-    if [ $? -ne 0 ]; then
-        head=`head -n1 $scriptdir/issue_list.txt`
-        python3 notice_error.py issue -e "$response" -i "$head"
-        echo $head >> $scriptdir/error/issue_list.txt
-        cat $title_path > $scriptdir/error/$title_file_name
-        cat $file_path > $scriptdir/error/$body_file_name
     fi
 
-    echo $responce
+    if [[ "$line" != *REPORTED* ]]; then
+        mkdir -p $scriptdir/content
+        mkdir -p $scriptdir/error
+
+        title_path=$scriptdir/content/$title_file_name
+        file_path=$scriptdir/content/$body_file_name
+        response=`python3 make_issue.py -t $title_path -f $file_path -u ${url[@]} -l ${log_name[@]}`
+
+        if [ $? -ne 0 ]; then
+            head=`head -n1 $scriptdir/issue_list.txt`
+            python3 notice_error.py issue -e "$response" -i "$head"
+            echo $head >> $scriptdir/error/issue_list.txt
+            cat $title_path > $scriptdir/error/$title_file_name
+            cat $file_path > $scriptdir/error/$body_file_name
+        else
+            sed -i "s/\(.*$log\)/\1,REPORTED/" $scriptdir/issue_list.txt
+        fi
+
+        echo $response
+    fi
     
     sed -i "s/\(.*$log\)/\1,UPLOADED/" $scriptdir/issue_list.txt
 done
