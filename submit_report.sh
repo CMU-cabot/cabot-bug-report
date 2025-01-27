4931ad3a7c7c400589ae0a8035891cb8eaf221f8
fukimiyazaki <f2miyazaki@lab.miraikan.jst.go.jp>
2025-01-27 10:49:53 +0900

    prioritize the specified network
    Signed-off-by: fukimiyazaki <f2miyazaki@lab.miraikan.jst.go.jp>

--- submit_report.sh
+++ submit_report.sh
@@ -10,12 +10,17 @@ logdir="$cabotdir/docker/home/.ros/log"
 
 ssid=`iwgetid -r`
 can_upload=0
+METRIC=50
 
 export $(cat $scriptdir/.env | grep -v "#" | xargs)
 
 if [ -z "$ssid" ]; then
     exit
 elif [ $ssid == $SSID ]; then
+    if [ -n "$DROUTE" ]; then
+        sudo ip route del default via $DROUTE
+        sudo ip route add default via $DROUTE metric $METRIC
+    fi
     can_upload=1
 else
     bash $scriptdir/notification.sh $CABOT_NAME" M-lab以外接続時にtimerが終了するか確認通知"
@@ -203,6 +208,10 @@ if [ $failed -eq 1 ]; then
 elif [ $can_upload -eq 1 ]; then
     bash $scriptdir/notification.sh $CABOT_NAME"の自動アップロードを終了します。"
     systemctl --user stop submit_report.timer
+    if [ -n "$DROUTE" ]; then
+        sudo ip route del default via $DROUTE
+        sudo ip route add default via $DROUTE metric 600
+    fi
 fi
 
 [ -f stdout.log ] && rm stdout.log
