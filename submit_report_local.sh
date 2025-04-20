e31bc69648f107b6e7de26ea561ffe02d4c96aa0
MIYAZAKI Fuki/宮崎 楓己 <miyazakf203@Mlab17.local>
2025-04-20 11:51:47 +0900

    send tar file with rsync
    Signed-off-by: MIYAZAKI Fuki/宮崎 楓己 <miyazakf203@Mlab17.local>

--- submit_report_local.sh
+++ submit_report_local.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+terminating=0
+
+cleanup() {
+    echo "Caught signal, cleaning up..."
+    terminating=1
+    sleep 1
+    exit 1
+}
+trap cleanup SIGINT SIGTERM SIGHUP
+
 pwd=`pwd`
 scriptdir=`dirname $0`
 cd $scriptdir
@@ -9,11 +19,34 @@ logdir="/opt/cabot/docker/home/.ros/log"
 source $scriptdir/.env
 
 sudo mkdir -p /mnt/smbshare
-sudo mount -t cifs -o username=$NAS_USER,password=$NAS_PASSWORD,uid=$HOST_UID,gid=$HOST_GID,file_mode=0664,dir_mode=0755 //$NAS_IP/$NAS_SHARE_DIR /mnt/smbshare
+sudo mount -t cifs -o username=$NAS_USER,password=$NAS_PASSWORD,uid=$HOST_UID,gid=$HOST_GID,cache=none,file_mode=0664,dir_mode=0755 //$NAS_IP/$NAS_SHARE_DIR /mnt/smbshare
 
-rsync -av $scriptdir /mnt/smbshare/$CABOT_NAME/
-date=$(date "+%Y-%m-%d")
-items=($(ls $logdir | grep "cabot_${date}" | grep -v .tar))
+# Parse options
+while getopts "d:h" opt; do
+    case $opt in
+        d) date=$OPTARG ;;
+        h) 
+            echo "Usage: $0 [-d date] [-h]"
+            echo "  -d date   Specify the date in YYYY-MM-DD format (default: today's date)"
+            echo "  -h        Show this help message"
+            exit 0
+            ;;
+        *) 
+            echo "Invalid option. Use -h for help."
+            exit 1
+            ;;
+    esac
+done
+
+# Default to today's date if not specified
+if [ -z "$date" ]; then
+    date=$(date "+%Y-%m-%d")
+fi
+
+rsync -av $scriptdir/content /mnt/smbshare/$CABOT_NAME/
+rsync -av $scriptdir/issue_list.txt /mnt/smbshare/$CABOT_NAME/
+
+items=($(ls $logdir | grep "cabot_${date}" | grep -v .tar | grep -v _part_))
 for item in ${items[@]}
 do
     echo $item
@@ -21,18 +54,27 @@ do
     SIZE=`du -d 0 $item | cut -f 1`
 
     FILE1="${item}_log.tar"
-    if [ ! -e /mnt/smbshare/$CABOT_NAME/log/$FILE1 ]; then
-        tar --exclude="ros2_topics" --exclude="image_topics" -cvf /mnt/smbshare/$CABOT_NAME/log/$FILE1 $item
+    if [ ! -e $FILE1 ]; then
+        tar --exclude="ros2_topics" --exclude="image_topics" -cvf $FILE1 $item
     fi
+    tars=($FILE1)
     if [ $SIZE -gt 13000000 ]; then
-        PARTS=(${item}_ros2_topics_part_*)
-        if [ ! -e "/mnt/smbshare/$CABOT_NAME/log/${PARTS[0]}" ]; then
-            tar -cvf - $item/ros2_topics | split -b 10G - /mnt/smbshare/$CABOT_NAME/log/${item}_ros2_topics_part_
+        PARTS=($(ls $logdir | grep ${item}_ros2_topics_part_))
+        if [ ! -e "${PARTS[0]}" ]; then
+            tar -cvf - $item/ros2_topics | split -b 10G - ${item}_ros2_topics_part_
         fi
+        tars+=(`ls | grep ${item}_ros2_topics_part_`)
     else
         FILE2="${item}_ros2_topics.tar"
-        if [ ! -e /mnt/smbshare/$CABOT_NAME/log/$FILE2 ]; then
-            tar -cvf /mnt/smbshare/$CABOT_NAME/log/$FILE2 $item/ros2_topics
+        if [ ! -e $FILE2 ]; then
+            tar -cvf $FILE2 $item/ros2_topics
         fi
+        tars+=($FILE2)
+    fi
+    echo ${tars[@]}
+    echo rsync start
+    rsync -av --size-only "${tars[@]}" /mnt/smbshare/$CABOT_NAME/log/ 
+    if [[ $terminating -eq 1 ]]; then
+        break
     fi
 done
